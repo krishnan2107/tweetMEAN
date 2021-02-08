8b0dbcced7d34041c9f31bff77c7b43ff504abb5
Chris Morris <github@morrisdigital.co.uk>
2021-02-08 10:11:01 +0000

    expose port 8081 for management, get mongo and angular talking

--- NodeReader/db.js
+++ NodeReader/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/tweets', {useNewUrlParser:true, useUnifiedTopology:true}, (err) => {
+mongoose.connect('mongodb://mongo:27017/tweets', {useNewUrlParser:true, useUnifiedTopology:true}, (err) => {
   if(!err)
     console.log("Mongo DB Connected");
   else
